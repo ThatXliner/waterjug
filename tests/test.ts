1d190367f2779ba43dca7d88967868972e551fb7
ThatXliner <bryan.hu.2020@gmail.com>
2023-11-15 12:43:14 -0800

    Update test.ts

--- tests/test.ts
+++ tests/test.ts
@@ -2,5 +2,4 @@ import { expect, test } from '@playwright/test';
 // TODO: full integration testing
 test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: /R区N片r/i })).toBeVisible();
 });
