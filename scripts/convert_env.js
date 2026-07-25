9b6d893411cbf3dfc0af3306b6454ca1e5caf581
ThatXliner <bryan.hu.2020@gmail.com>
2026-07-24 23:22:52 -0700

    Use stable local Supabase settings in CI

--- scripts/convert_env.js
+++ scripts/convert_env.js
@@ -12,29 +12,18 @@ const readFilePromise = (...args) =>
 	});
 
 const stdin = await readFilePromise(process.stdin.fd, 'utf-8');
+console.log(stdin);
 const REMAP = {
 	API_URL: 'PUBLIC_SUPABASE_URL',
 	ANON_KEY: 'PUBLIC_SUPABASE_ANON_KEY',
 	SERVICE_ROLE_KEY: 'SUPABASE_SERVICE_ROLE_KEY'
 };
-const output = new Map();
-
 for (let keyValue of stdin.matchAll(/(\w+)="(.+)"/gm)) {
 	const key = keyValue[1];
 	const value = keyValue[2];
 	if (Object.prototype.hasOwnProperty.call(REMAP, key)) {
-		output.set(REMAP[key], value);
+		console.log(`${REMAP[key]}=${value}`);
 	} else {
-		output.set(key, value);
+		console.log(`${key}=${value}`);
 	}
-}
-
-for (const requiredKey of Object.values(REMAP)) {
-	if (!output.has(requiredKey)) {
-		throw new Error(`Supabase status did not provide ${requiredKey}`);
-	}
-}
-
-for (const [key, value] of output) {
-	console.log(`${key}=${value}`);
 }
